acb7b3a649e8e4bf7fb62f06b04b5f5d7ddd27f6
[Kelly Gerstner] <[kellymgerstner@gmail.com]>
2021-12-06 09:51:03 -0600

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupportServicesSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger>)
+        {
+            _logger = logger;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResultPrivacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = ActivityTrackingOptions.Current ? IDesignTimeMvcBuilderConfiguration ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
